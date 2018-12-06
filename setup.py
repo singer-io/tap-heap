cafe858a0f8e2ef8cc0bbdc4149d855b1b7a45a3
Kyle Allan <kyle@stitchdata.com>
2018-12-06 17:07:49 +0000

    bump to 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-heap',
-      version='0.0.4',
+      version='0.0.5',
       description='Singer.io tap for extracting Heap data from Avro files in S3',
       author='Stitch',
       url='https://singer.io',
