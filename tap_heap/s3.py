708cf3d53b072444eff4ae9dbe564330b4ba092d
Kyle Allan <KAllan357@gmail.com>
2018-12-06 12:06:53 -0500

    Fix Expiring Credentials (#5)
    * fix expiring credentials
    
    * refresh credentials

--- tap_heap/s3.py
+++ tap_heap/s3.py
@@ -1,14 +1,22 @@
 import re
 import backoff
 import boto3
-import botocore
 import singer
+
+from botocore.credentials import (
+    AssumeRoleCredentialFetcher,
+    CredentialResolver,
+    DeferredRefreshableCredentials,
+    JSONFileCache
+)
+from botocore.exceptions import ClientError
+from botocore.session import Session
 
 LOGGER = singer.get_logger()
 
 def retry_pattern():
     return backoff.on_exception(backoff.expo,
-                                botocore.exceptions.ClientError,
+                                ClientError,
                                 max_tries=5,
                                 on_backoff=log_backoff_attempt,
                                 factor=10)
@@ -19,19 +27,44 @@ def log_backoff_attempt(details):
                 details.get("tries"))
 
 
+class AssumeRoleProvider():
+    METHOD = 'assume-role'
+
+    def __init__(self, fetcher):
+        self._fetcher = fetcher
+
+    def load(self):
+        return DeferredRefreshableCredentials(
+            self._fetcher.fetch_credentials,
+            self.METHOD
+        )
+
+
 @retry_pattern()
 def setup_aws_client(config):
-    client = boto3.client('sts')
     role_arn = "arn:aws:iam::{}:role/{}".format(config['account_id'].replace('-', ''),
                                                 config['role_name'])
+    session = Session()
+    fetcher = AssumeRoleCredentialFetcher(
+        session.create_client,
+        session.get_credentials(),
+        role_arn,
+        extra_args={
+            'DurationSeconds': 3600,
+            'RoleSessionName': 'TapHeap',
+            'ExternalId': config['external_id']
+        },
+        cache=JSONFileCache()
+    )
+
+    refreshable_session = Session()
+    refreshable_session.register_component(
+        'credential_provider',
+        CredentialResolver([AssumeRoleProvider(fetcher)])
+    )
 
     LOGGER.info("Attempting to assume_role on RoleArn: %s", role_arn)
-    role = client.assume_role(RoleArn=role_arn,
-                              ExternalId=config['external_id'],
-                              RoleSessionName='TapHeap')
-    boto3.setup_default_session(aws_access_key_id=role['Credentials']['AccessKeyId'],
-                                aws_secret_access_key=role['Credentials']['SecretAccessKey'],
-                                aws_session_token=role['Credentials']['SessionToken'])
+    boto3.setup_default_session(botocore_session=refreshable_session)
 
 
 @retry_pattern()
