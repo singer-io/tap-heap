934a3ecbfb990c1080c38c1aa345c7b5512fd763
Andy Lu <andy@stitchdata.com>
2019-02-05 18:07:08 +0000

    Bump version to 0.0.7

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 0.0.7
+  * Use `filter_data_by_metadata()` instead of `transform()` to speed up record throughput [#7](https://github.com/singer-io/tap-heap/pull/7)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-heap',
-      version='0.0.6',
+      version='0.0.7',
       description='Singer.io tap for extracting Heap data from Avro files in S3',
       author='Stitch',
       url='https://singer.io',
