7c875524c3207a5e5504d3dec18c545a4a20eb6d
Atul Tiwari <atul.tiwari@qlik.com>
2026-06-08 17:41:47 +0530

    Update setup.py for install dependencies version upgrade

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup
 
 setup(name='tap-heap',
-      version='1.5.0',
+      version='1.6.0',
       description='Singer.io tap for extracting Heap data from Avro files in S3',
       author='Stitch',
       url='https://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_heap'],
       install_requires=[
-          'boto3==1.39.9',
-          'singer-encodings==0.1.3',
+          'boto3==1.43.24',
+          'singer-encodings==0.5.0',
           'singer-python==6.8.0',
           'python-snappy==0.7.3',
-          'fastavro==1.11.1'
+          'fastavro==1.12.2'
       ],
       extras_require={
           'dev': [
